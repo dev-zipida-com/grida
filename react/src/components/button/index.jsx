46d3811b9342262bc9139a457d32b43f10602efd
YongjunKang <yj.kang@zipida.com>
2023-03-20 19:15:23 +0900

    Add Figma HTML/CSS

--- react/src/components/button/index.jsx
+++ react/src/components/button/index.jsx
@@ -1,7 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import yaml from 'js-yaml';
 import './index.css';
+import defaultProps from './button.yaml';
 
-const Button = ({ children, ...args }) => {
-  return <button {...args}>{children ?? 'Button'}</button>;
+const defaultPropValues = yaml.load(defaultProps);
+
+const Button = (props) => {
+  const { type, onClick, disabled, children, ...args } = props;
+  const buttonClass = `grida-button ${type}`;
+  const buttonTextClass = `grida-button-text ${type}`;
+
+  return (
+    <button
+      className={buttonClass}
+      disabled={disabled}
+      onClick={onClick}
+      {...args}
+    >
+      <span className={buttonTextClass}>{children}</span>
+    </button>
+  );
 };
+
+Button.propTypes = {
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = defaultPropValues;
 
 export default Button;
